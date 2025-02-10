05da65837d85eb3ff2d88d207ac48989e638cd97
Nati <tusikf@yandex.ru>
2025-02-10 14:26:19 +0300

    done

---
+++ app/src/main/java/ms/example/hw_borozinets_10/MainActivity.kt
@@ -0,0 +1,27 @@
+package ms.example.hw_borozinets_10
+
+import android.os.Bundle
+import android.text.TextUtils.replace
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        supportFragmentManager.commit {
+            replace<FirstBlankFragment>(R.id.fragmentinactivity)
+            addToBackStack(FirstBlankFragment::class.java.simpleName)
+        }
+    }
+}
